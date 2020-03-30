826a1d7c1b97e15186834a552a75babcc9559ce1
soulwine-it <skyorwind@gmail.com>
2020-03-30 18:57:42 +0900

    two Inpnut

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,49 +1,43 @@
 import React, { useState } from "react";
 
 function InputSample() {
-  const [text, setText] = useState("");
+  const [inputs, setInputs] = useState({
+    name: "",
+    nickname: ""
+  });
+  const { name, nickname } = inputs;
   const onChange = e => {
-    setText(e.target.value);
+    const { name, value } = e.target;
+    //객체 상태를 업데이트 할때에는 객체를 복사하고 나서 덮어씌우고 업데이트 해줘야 한다.
+
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
   };
 
   const onReset = () => {
-    setText("");
+    setInputs({
+      name: "",
+      nickname: ""
+    });
   };
   return (
     <div>
-      <input onChange={onChange} value={text} />
+      <input name="name" onChange={onChange} placeholder="이름" value={name} />
+      <input
+        name="nickname"
+        placeholder="닉네임"
+        onChange={onChange}
+        value={nickname}
+      />
       <button onClick={onReset}>초기화</button>
       <div>
-        <b>값: {text}</b>
+        <b>값: 닉네임</b>
+        {name} ({nickname})
       </div>
     </div>
   );
 }
 
 export default InputSample;
-
-// import React, { useState } from "react";
-
-// function InputSample() {
-//   const [text, setText] = useState("");
-
-//   const onChange = e => {
-//     setText(e.target.value);
-//   };
-
-//   const onReset = () => {
-//     setText("");
-//   };
-//   return (
-//     <div>
-//       <input onChange={onChange} value={text} />
-//       <button onClick={onReset}>초기화</button>
-//       <div>
-//         <b>값: </b>
-//         {text}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default InputSample;
