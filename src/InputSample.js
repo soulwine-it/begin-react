def1e7195bb890f8a5b8bd36ed315a8b96d301c1
soulwine-it <skyorwind@gmail.com>
2020-03-30 18:35:38 +0900

    Update InputSample.js

--- src/InputSample.js
+++ src/InputSample.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 
 function InputSample() {
   const [text, setText] = useState("");
-
   const onChange = e => {
     setText(e.target.value);
   };
@@ -15,11 +14,36 @@ function InputSample() {
       <input onChange={onChange} value={text} />
       <button onClick={onReset}>초기화</button>
       <div>
-        <b>값: </b>
-        {text}
+        <b>값: {text}</b>
       </div>
     </div>
   );
 }
 
 export default InputSample;
+
+// import React, { useState } from "react";
+
+// function InputSample() {
+//   const [text, setText] = useState("");
+
+//   const onChange = e => {
+//     setText(e.target.value);
+//   };
+
+//   const onReset = () => {
+//     setText("");
+//   };
+//   return (
+//     <div>
+//       <input onChange={onChange} value={text} />
+//       <button onClick={onReset}>초기화</button>
+//       <div>
+//         <b>값: </b>
+//         {text}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default InputSample;
