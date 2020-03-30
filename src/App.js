1024b4c0562a9a1e4b72c4586f3078167e90ec93
soulwine-it <skyorwind@gmail.com>
2020-03-30 15:51:45 +0900

    {children}

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React from "react";
 import Hello from "./Hello.js";
 import "./App.css";
-function App() {
-  const name = "react";
-  const style = {
-    backgroundColor: "black",
-    color: "aqua",
-    fontSize: 30,
-    padding: "1rem"
-  };
-  return (
-    <>
-      <Hello />
+import Wrapper from "./Wrapper.js";
 
-      <div style={style}>{name}</div>
-      <div className="gray-box"></div>
-    </>
+function App() {
+  return (
+    <Wrapper>
+      <Hello name="react" color="red" />
+      <Hello color="pink" />
+    </Wrapper>
   );
 }
 
--- src/Hello.js
+++ src/Hello.js
@@ -1,8 +1,31 @@
 import React from "react";
 
 //첫번째 컴퍼넌트 생성
-function Hello() {
-  return <div>안녕하세요</div>;
+function Hello(props) {
+  return (
+    <div
+      style={{
+        color: props.color
+      }}
+    >
+      안녕하세요{props.name}
+    </div>
+  );
 }
+// 비구조화 할당시
+// function Hello({color, name}) {
+//     return (
+//       <div
+//         style={{
+//           color
+//         }}
+//       >
+//         안녕하세요{name}
+//       </div>
+//     );
+//   }
 
+Hello.defaultProps = {
+  name: "이름없음"
+};
 export default Hello;
