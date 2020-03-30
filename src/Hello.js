b04db02e6a4820186e7cb96f6475b1c6bd92c757
soulwine-it <skyorwind@gmail.com>
2020-03-30 16:00:04 +0900

    조건부 렌더링

--- src/Hello.js
+++ src/Hello.js
@@ -1,29 +1,32 @@
 import React from "react";
 
 //첫번째 컴퍼넌트 생성
-function Hello(props) {
+// function Hello(props) {
+//   return (
+//     <div
+//       style={{
+//         color: props.color
+//       }}
+//     >
+//       안녕하세요{props.name}
+//     </div>
+//   );
+// }
+// 비구조화 할당시
+function Hello({ color, name, isSpecial }) {
   return (
     <div
       style={{
-        color: props.color
+        color
       }}
     >
-      안녕하세요{props.name}
+      {/* isSpecial값이 true면 * 추가 아니면 null*/}
+      {isSpecial ? <b>*</b> : null}
+      {/* {isSpecial && <b>*</b>} */}
+      안녕하세요{name}
     </div>
   );
 }
-// 비구조화 할당시
-// function Hello({color, name}) {
-//     return (
-//       <div
-//         style={{
-//           color
-//         }}
-//       >
-//         안녕하세요{name}
-//       </div>
-//     );
-//   }
 
 Hello.defaultProps = {
   name: "이름없음"
